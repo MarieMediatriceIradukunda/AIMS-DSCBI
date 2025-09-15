38dc69c89e6a6998082e98246b2be2b907dec6fc
Marie Mediatrice Iradukunda <mariemdeiatrice.iradukunda@rra.gov.rw>
2025-09-15 17:00:27 +0200

    Updated data imports.

--- sql/data_import.sql
+++ sql/data_import.sql
@@ -78,3 +78,17 @@ DELIMITER ',' CSV HEADER;
                   pixel_count, raster_filename, year, month, date)
 FROM '/home/ira2351696/Desktop/AIMS-DSCBI/data/tmp-db-data/ntl-monthly-2012-2024.csv'
 CSV HEADER;
+
+COPY pop(cell_id, elderly_60, children_under5, youth_15_24, general_pop, men_2020, women_2020, building_count)
+FROM '/home/ira2351696/Desktop/AIMS-DSCBI/data/tmp-db-data/population.csv'
+CSV HEADER;
+
+COPY ntl_annual (cell_id, ntl_min, ntl_max, ntl_mean, ntl_median, ntl_sum,
+                  pixel_count, raster_filename, year)
+FROM '/home/ira2351696/Desktop/AIMS-DSCBI/data/tmp-db-data/ntl-annual-2012-2024.csv' 
+DELIMITER ',' CSV HEADER;
+
+\COPY ntl_monthly(cell_id, ntl_min, ntl_max, ntl_mean, ntl_median, ntl_sum,
+                  pixel_count, raster_filename, year, month, date)
+FROM '/home/ira2351696/Desktop/AIMS-DSCBI/data/tmp-db-data/ntl-monthly-2012-2024.csv'
+CSV HEADER;
